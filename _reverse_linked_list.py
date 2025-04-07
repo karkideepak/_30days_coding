10a6403a3cdd1f7f4cbafef9c6f0bc81eefdddb1
Deepak <102180983+karkideepak@users.noreply.github.com>
2025-04-06 23:04:00 -0500

    Update _reverse_linked_list.py

--- _reverse_linked_list.py
+++ _reverse_linked_list.py
@@ -1,18 +1,23 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Base case: If the list is empty, return None
         if not head:
             return None
 
+        # Assume the first node is the new head initially
         newHead = head
+
+        # If there is a next node, we keep reversing
         if head.next:
+            # Recursively reverse the rest of the list
             newHead = self.reverseList(head.next)
+
+            # Reverse the link:
+            # The next node's 'next' should point back to the current node
             head.next.next = head
+
+        # After reversal, the current node should not point to anything
         head.next = None
-        
+
+        # Return the new head of the reversed list
         return newHead
